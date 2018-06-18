3d57ab572983b2257be6406f14b51bb248503954
Victor Li <victorli5674@yahoo.com>
2018-06-18 00:38:30 -0700

    rendering fetched data from graphql endpoint in client

--- client/examples/App.js
+++ client/examples/App.js
@@ -1,45 +1,77 @@
 // Components
 const { CourseHeader, LessonHeader, Material } = components();
 
-fetch('/data')
-  .then(response => response.json())
-  .then(data => {
-    const { name: degreeName, courses } = data;
+function renderData(data) {
+  const { name: degreeName, courses } = data;
 
-    const Content = courses.map(({ name, lessons }) => {
-      return MyReact.createElement(
-        'div',
-        null,
-        MyReact.createElement(CourseHeader, { name }),
-        MyReact.createElement(
-          'div',
-          null,
-          lessons.map(({ name, materials }) => {
-            return MyReact.createElement(
-              'details',
-              null,
-              MyReact.createElement(LessonHeader, { name }),
-              materials.map(({ name, type, link, question, choices, answer }) =>
-                MyReact.createElement(Material, {
-                  name,
-                  type,
-                  link,
-                  question,
-                  choices,
-                  answer
-                })
-              )
-            );
-          })
-        )
-      );
-    });
-
-    const App = MyReact.createElement(
+  const Content = courses.map(({ name, lessons }) => {
+    return MyReact.createElement(
       'div',
       null,
-      MyReact.createElement('h1', null, degreeName),
-      Content
+      MyReact.createElement(CourseHeader, { name }),
+      MyReact.createElement(
+        'div',
+        null,
+        lessons.map(({ name, materials }) => {
+          return MyReact.createElement(
+            'details',
+            null,
+            MyReact.createElement(LessonHeader, { name }),
+            materials.map(({ name, type, link, question, choices, answer }) =>
+              MyReact.createElement(Material, {
+                name,
+                type,
+                link,
+                question,
+                choices,
+                answer
+              })
+            )
+          );
+        })
+      )
     );
-    MyReact.render(App, document.getElementById('root'));
+  });
+
+  const App = MyReact.createElement(
+    'div',
+    null,
+    MyReact.createElement('h1', null, degreeName),
+    Content
+  );
+  MyReact.render(App, document.getElementById('root'));
+}
+
+// fetch('/data')
+//   .then(response => response.json())
+//   .then(renderData);
+
+fetch('/graphql', {
+  method: 'POST',
+  headers: { 'Content-Type': 'application/json' },
+  body: JSON.stringify({
+    query: `{
+      degree(id: 0) {
+        name
+        courses {
+          name
+          lessons {
+            name
+            materials {
+              name
+              type
+              link
+              question
+              choices
+              answer
+            }
+          }
+        }
+      }
+    }`
+  })
+})
+  .then(res => res.json())
+  .then(({ data }) => {
+    renderData(data.degree);
   });
